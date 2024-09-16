de43536471960a6456f6cfb9fdb2161fb3544e06
Pedro Oliveira <012pedropaulo@gmail.com>
2024-09-16 13:19:25 -0300

    feat: Add login banner component
    Add login-banner.component.html, login-banner.component.scss, and login-banner.component.ts files to implement a login banner component. The component includes a title and an image for the login page.

---
+++ src/app/components/login-banner/login-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-banner.component.html',
+  styleUrl: './login-banner.component.scss'
+})
+export class LoginBannerComponent {
+
+}
