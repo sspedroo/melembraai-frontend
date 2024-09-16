713a3a52813e4fe73f4f64af11a11ac845ff27f2
Pedro Oliveira <012pedropaulo@gmail.com>
2024-09-16 13:18:59 -0300

    feat: Add login route to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ConfirmRegisterComponent } from './pages/confirm-register/confirm-register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'terms', component: TermsComponent},
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'confirm-email', component: ConfirmRegisterComponent},
 ];
